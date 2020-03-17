95ad20c38223fd95964d417f29ddb7b28985f7f4
סול אילן <solipod8@gmail.com>
2020-03-17 17:33:56 +0200

    add node server for deployment

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("./dist/tattoos-confessions-angular"));
+app.get("/*", function(req, res) {
+  res.sendFile("index.html", { root: "dist/tattoos-confessions-angular/" });
+});
+app.listen(process.env.PORT || 8080);
