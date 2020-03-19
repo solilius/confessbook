f574794b5851a585007e2fbf58a595783c097840
סול אילן <solipod8@gmail.com>
2020-03-19 02:22:06 +0200

    remove unnecessary data from environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  server: "",
-  appName: "<temp_name>",
-  intro: "<temp_intro>"
+  server: ""
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: false,
-  server: "http://localhost:8080",
-  appName: "Confessions",
-  intro: "זה המקום להתוודות על וידיים ש"
+  server: "http://localhost:8080"
 };
 
