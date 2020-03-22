ab5f7aee64b9bfeba42c9a88e10fb22829551d46
סול אילן <solipod8@gmail.com>
2020-03-22 02:23:00 +0200

    .

--- middlewares/confession.validator.js
+++ middlewares/confession.validator.js
@@ -1,5 +1,5 @@
 const Joi = require("joi");
-Joi.objectId = require("Joi-objectid")(Joi);
+Joi.objectId = require("joi-objectid")(Joi);
 
 const messageScheme = Joi.object().keys({
   message: Joi.string()
