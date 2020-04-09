fa65945411e84b7698cebde070b0a7c71ecfba3a
סול אילן <solipod8@gmail.com>
2020-04-09 18:10:53 +0300

    scroll when last item clicked

--- src/app/components/manager/child-components/main/main.component.ts
+++ src/app/components/manager/child-components/main/main.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ConfessionsService } from '../../../../services/confessions.service';
+import { SchedulersService } from '../../../../services/schedulers.service';
 import { Confession } from '../../../../models/confession/confession.module'
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
@@ -13,11 +14,11 @@ import Swal from 'sweetalert2';
 export class MainComponent implements OnInit {
     confessions: Confession[];
 
-    constructor(private confessionsService: ConfessionsService, private router: Router) { }
+    constructor(private confessionsService: ConfessionsService, private schedulersService: SchedulersService, private router: Router) { }
     ngOnInit(): void {
         this.confessions = [];
         this.confessionsService.getConfessions(false).subscribe(confessions => {
-            this.confessionsService.allTags = this.getAllTags(confessions);
+            this.schedulersService.allTags = this.getAllTags(confessions);
             this.confessions = confessions;
         }, (err) => {
             console.log(err);
@@ -34,11 +35,18 @@ export class MainComponent implements OnInit {
         this.confessions = this.confessions.filter(c => c._id !== id);
     }
 
-    getAllTags(confessions:Confession[]): string[]{
+    getAllTags(confessions: Confession[]): string[] {
         let tags = [];
-        confessions.forEach(confession =>{
+        confessions.forEach(confession => {
             tags = tags.concat(confession.tags);
         });
-        return tags.filter((item, index) => {return tags.indexOf(item) == index});
+        return tags.filter((item, index) => { return tags.indexOf(item) == index });
+    }
+
+    lastItemClicked(id) {
+        if (id === this.confessions[this.confessions.length - 1]._id) {
+            const objDiv = document.getElementById("confession-list");
+            objDiv.scrollTop = objDiv.scrollHeight;
+        }
     }
 }
